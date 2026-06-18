c3fff700895ca0261b2079a8f17d63453dd04b79
Treighton Mauldin <treighton@gmail.com>
2026-06-18 09:48:32 -0700

    feat: export sync protocol and session public surface

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,15 @@ export {
   type PersistenceTimerDeps,
 } from "./indexedDbPersistence.js";
 export { type ChangeListener, type ChangeOrigin } from "./featureStore.js";
+export {
+  idsNeeded,
+  parseFeature,
+  parseMessage,
+  type SyncMessage,
+} from "./syncProtocol.js";
+export {
+  connectionPair,
+  type Connection,
+  type InMemoryConnection,
+} from "./connection.js";
+export { SyncSession } from "./syncSession.js";
