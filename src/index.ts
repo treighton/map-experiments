be86da20b09f660f6b1fd44a2d60ba138528e79c
Treighton Mauldin <treighton@gmail.com>
2026-06-17 20:38:57 -0700

    feat: finalize feature-store public exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 export * from "./types.js";
 export { newId } from "./uuid.js";
+export { mergeFeature, mergeAll } from "./merge.js";
+export {
+  FeatureStore,
+  type Identity,
+  type CreateInput,
+  type EditableFields,
+  type StoreDeps,
+} from "./featureStore.js";
+export {
+  loadOrCreateIdentity,
+  type KeyValueStore,
+} from "./identity.js";
